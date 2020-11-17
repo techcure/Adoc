4ba639ea870f5d9661d3a9acb7549d1cc8290b3d
techcure <gpendare@gmail.com>
2020-11-17 16:22:33 +0530

    pdf parce

--- scripts/cam.py
+++ scripts/cam.py
@@ -1,9 +1,9 @@
-import pygame
-import pygame.camera
 
-pygame.camera.init()
-pygame.camera.list_cameras() #Camera detected or not
-cam = pygame.camera.Camera("/dev/video0",(640,480))
-cam.start()
-img = cam.get_image()
-pygame.image.save(img,"filename.jpg")
+import PyPDF2 
+
+pdfFileObj = open('sample.pdf', 'rb') 
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
+print(pdfReader.numPages) 
+pageObj = pdfReader.getPage(0) 
+print(pageObj.extractText())  
+pdfFileObj.close() 
