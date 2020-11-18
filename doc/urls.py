34d8705fb833e98c8bfdd2f1e32647a5be72185c
techcure <gpendare@gmail.com>
2020-11-18 13:07:44 +0530

    fix views

--- doc/urls.py
+++ doc/urls.py
@@ -2,12 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from . import views
+from .views import *
+from django.views import View
 
 
 urlpatterns = [
    
-       path('', views.index, name='index'),
+       path('', IndexView.as_view(), name='index'),
 ]
-
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- doc/views.py
+++ doc/views.py
@@ -1,21 +1,37 @@
 from django.shortcuts import render
 from .forms import RegForm
+from django.http import HttpResponse
 from django.contrib import messages
 from .models import *
+from django.views import View
 from django.contrib.auth.decorators import login_required
 
 
+class IndexView(View):
 # Create your views here.
-def index(request):
-	if request.method == 'POST':
-		form = RegForm(request.POST, request.FILES)
-		if form.is_valid():
-			post = form.save(commit=True)
-			post.save()
-			messages.success(request, 'Success!.')
-		return render(request, 'doc/index.html', {'form': form})
-	else:
+	def post(self, request, *args, **kwargs):
+		if request.method == 'POST':
+			form = RegForm(request.POST, request.FILES)
+			if form.is_valid():
+				post = form.save(commit=True)
+				post.save()
+				messages.success(request, 'Success!.')
+			return render(request, 'doc/index.html', {'form': form})
+		else:
 			form = RegForm()
 			queryset = Reg.objects.all()
 
-	return render(request, 'doc/index.html', {'queryset':queryset, 'form':form})
+		return render(request, 'doc/index.html', {'queryset':queryset, 'form':form})
+	def get(self, request, *args, **kwargs):
+		if request.method == 'POST':
+			form = RegForm(request.POST, request.FILES)
+			if form.is_valid():
+				post = form.save(commit=True)
+				post.save()
+				messages.success(request, 'Success!.')
+			return render(request, 'doc/index.html', {'form': form})
+		else:
+			form = RegForm()
+			queryset = Reg.objects.all()
+
+		return render(request, 'doc/index.html', {'queryset':queryset, 'form':form})
