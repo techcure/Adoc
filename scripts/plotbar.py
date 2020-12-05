eb6c7b11cb4d1db4afadc909075c56b049d1fb8e
techcure <gpendare@gmail.com>
2020-12-05 20:53:19 +0530

    add script

---
+++ scripts/plotbar.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# x = ['A', 'B', 'C']
+# y = [1, 5, 3]
+
+# plt.bar(x, y)
+# plt.show()
+
+# x = ['A', 'B', 'C']
+# y = [1, 5, 3]
+
+# plt.barh(x, y)
+# plt.show()
+
+x = ['A', 'B', 'C']
+y = [1, 5, 3]
+
+plt.bar(x, y, color=['red', 'blue', 'green'])
+plt.show()
